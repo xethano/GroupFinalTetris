d90e4c8ec62755b86367eb62ae8ae37fd3384c60
xethano <48847303+xethano@users.noreply.github.com>
2023-11-30 23:44:47 -0800

    library stuff
    yup

--- GroupFinalTetris.cpp
+++ GroupFinalTetris.cpp
@@ -4,17 +4,27 @@
 #include <iostream>
 #include "common.h"
 
-#pragma comment( lib, "sfml-main-s-d.lib")
-#pragma comment( lib, "sfml-graphics-s-d.lib")
-#pragma comment( lib, "sfml-system-s-d.lib")
-#pragma comment( lib, "sfml-window-s-d.lib")
-#pragma comment( lib, "sfml-audio-s-d.lib")
+#if _DEBUG
+    // debug mode requires loading debug libraries from SMFL
+    // #pragma comment lib only works on windows compilers!
+    #pragma comment( lib, "sfml-main-s-d.lib")
+    #pragma comment( lib, "sfml-graphics-s-d.lib")
+    #pragma comment( lib, "sfml-system-s-d.lib")
+    #pragma comment( lib, "sfml-window-s-d.lib")
+    #pragma comment( lib, "sfml-audio-s-d.lib")
+#else
+    #pragma comment( lib, "sfml-main-s.lib")
+    #pragma comment( lib, "sfml-graphics-s.lib")
+    #pragma comment( lib, "sfml-system-s.lib")
+    #pragma comment( lib, "sfml-window-s.lib")
+    #pragma comment( lib, "sfml-audio-s.lib")
+#endif
+
 #pragma comment( lib, "vorbis.lib")
 #pragma comment( lib, "ogg.lib")
 #pragma comment( lib, "flac.lib")
 #pragma comment( lib, "openal32.lib")
 #pragma comment( lib, "vorbisfile.lib")
-
 #pragma comment( lib, "winmm.lib")
 #pragma comment( lib, "opengl32.lib")
 #pragma comment( lib, "freetype.lib")
